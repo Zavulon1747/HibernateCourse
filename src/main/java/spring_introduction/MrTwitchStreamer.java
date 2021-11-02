b2554c5cae1c6c225109601e4b6272d426d62999
Zavulon1747 <d1mas322@yandex.ru>
2021-11-02 17:38:02 +0400

    DI prototype and singleton scopes

--- src/main/java/spring_introduction/MrTwitchStreamer.java
+++ src/main/java/spring_introduction/MrTwitchStreamer.java
@@ -1,6 +1,15 @@
 package spring_introduction;
 
 public class MrTwitchStreamer implements TwitchAnimal {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
     @Override
     public void say() {
         System.out.println("ДО ПОБАЧЕНЯ");
---
+++ src/main/java/spring_introduction/Test4.java
@@ -0,0 +1,20 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+        MrTwitchStreamer twitchStreamer = context.getBean("myPet", MrTwitchStreamer.class);
+        twitchStreamer.setName("PAPICH");
+        MrTwitchStreamer yourTwitchStreamer = context.getBean("myPet", MrTwitchStreamer.class);
+        yourTwitchStreamer.setName("NIX");
+
+        System.out.println(twitchStreamer.getName());
+        System.out.println(yourTwitchStreamer.getName());
+
+        System.out.println(twitchStreamer.equals(yourTwitchStreamer));
+
+        context.close();
+    }
+}
