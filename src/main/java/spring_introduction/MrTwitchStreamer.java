24f318b4b8942937a110d74cbc784cd9b548e2ff
Zavulon1747 <d1mas322@yandex.ru>
2021-11-02 18:09:58 +0400

    DI prototype and singleton scopes use init and destroy methods

--- src/main/java/spring_introduction/MrTwitchStreamer.java
+++ src/main/java/spring_introduction/MrTwitchStreamer.java
@@ -1,6 +1,17 @@
 package spring_introduction;
 
+import java.util.Random;
+
 public class MrTwitchStreamer implements TwitchAnimal {
+    private String name;
+    public int countOfViewers;
+    Random random = new Random();
+
+    @Override
+    public void say() {
+        System.out.println("ДО ПОБАЧЕНЯ");
+    }
+
     public String getName() {
         return name;
     }
@@ -9,9 +20,36 @@ public class MrTwitchStreamer implements TwitchAnimal {
         this.name = name;
     }
 
-    private String name;
-    @Override
-    public void say() {
-        System.out.println("ДО ПОБАЧЕНЯ");
+    //For initialize some fields in game/application/web
+    private void init() {
+        countOfViewers = 0;
+        System.out.println("Стрим запускается. Количество зрителей на начале: "+countOfViewers);
+        for (int i = 3; i > 0; i--) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i);
+            countOfViewers= countOfViewers + random.nextInt(301);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Стрим запущен. Количество зрителей: "+countOfViewers);
+    }
+
+    //Throws some notes/information ...ets.
+    private void destroy() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        countOfViewers+= random.nextInt(1000);
+        say();
+        System.out.println("Стрим завершён. На момент оффа Вас смотрело: "+countOfViewers);
     }
 }
--- src/main/java/spring_introduction/Test4.java
+++ src/main/java/spring_introduction/Test4.java
@@ -6,14 +6,15 @@ public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
         MrTwitchStreamer twitchStreamer = context.getBean("myPet", MrTwitchStreamer.class);
-        twitchStreamer.setName("PAPICH");
-        MrTwitchStreamer yourTwitchStreamer = context.getBean("myPet", MrTwitchStreamer.class);
-        yourTwitchStreamer.setName("NIX");
+//        twitchStreamer.setName("PAPICH");
+//        MrTwitchStreamer yourTwitchStreamer = context.getBean("myPet", MrTwitchStreamer.class);
+//        yourTwitchStreamer.setName("NIX");
 
-        System.out.println(twitchStreamer.getName());
-        System.out.println(yourTwitchStreamer.getName());
-
-        System.out.println(twitchStreamer.equals(yourTwitchStreamer));
+//        System.out.println(twitchStreamer.getName());
+//        System.out.println(yourTwitchStreamer.getName());
+//
+//        System.out.println(twitchStreamer.equals(yourTwitchStreamer));
+//        twitchStreamer.say();
 
         context.close();
     }
